0a9cb127c6e9c6fb8e6dcc455d53bf78e80cf7f0
xiaoqi <253506624@qq.com>
2023-09-17 15:02:04 +0800

    feat: babel

--- create-react/babel.config.js
+++ create-react/babel.config.js
@@ -8,6 +8,8 @@ module.exports = {
       '@babel/preset-env',
       {
         targets: 'iOS 9, Android 4.4, last 2 versions, > 0.2%, not dead',
+        // 如果我们将参数项改成false，那么就不会对ES6模块化进行更改，还是使用import引入模块。使用ES6模块化语法有什么好处呢。在使用Webpack一类的打包工具，可以进行静态分析，从而可以做tree shaking 等优化措施。
+        modules: false,
         useBuiltIns: 'usage', // 会根据配置的目标环境找出需要的polyfill进行部分引入
         corejs: 3, // 使用 core-js@3 版本
       },
@@ -17,7 +19,12 @@ module.exports = {
   ],
   plugins: [
     isDEV && require.resolve('react-refresh/babel'), // 如果是开发模式,就启动react热更新插件
-
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        useESModules: true, // 不编译为commonjs
+      },
+    ],
     // ...
   ].filter(Boolean),
 }
